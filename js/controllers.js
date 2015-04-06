ecf7e067e1d61277e9969f541725b61d788beafb
Tushar <tushar@wohlig.com>
2015-04-07 03:17:47 +0530

    First Git

---
+++ js/controllers.js
@@ -0,0 +1,8 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+  
+})
+.controller('HomeCtrl', function($scope, $ionicModal, $timeout) {
+  
+});

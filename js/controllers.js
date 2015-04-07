f81f17cd2c2f9928e9cd3a08703210e857079e6d
Tushar <tushar@wohlig.com>
2015-04-07 20:02:20 +0530

    UI

--- js/controllers.js
+++ js/controllers.js
@@ -1,11 +1,41 @@
 angular.module('starter.controllers', [])
 
-.controller('LoginCtrl', function($scope, $ionicModal, $timeout) {
-  
-})
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-  
-})
+.controller('LoginCtrl', function($scope, $ionicModal, $timeout) {})
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {})
+
 .controller('HomeCtrl', function($scope, $ionicModal, $timeout) {
-  
+
+    $scope.predictions = [{
+        "team1": 60,
+        "venue": "Wankhade",
+        "date": "Wed, April 8",
+        "time": "20:00(IST)",
+        "logoteam1": "mumbai.png",
+        "logoteam2": "kkr.png"
+    }, {
+        "team1": 70,
+        "venue": "Banglore",
+        "date": "Wed, April 9",
+        "time": "20:00(IST)",
+        "logoteam1": "royal.png",
+        "logoteam2": "sunrise.png"
+    }, {
+        "team1": 80,
+        "venue": "Delhi",
+        "date": "Wed, April 10",
+        "time": "20:00(IST)",
+        "logoteam1": "delhi.png",
+        "logoteam2": "king.png"
+    }, {
+        "team1": 40,
+        "venue": "Pune",
+        "date": "Wed, April 11",
+        "time": "20:00(IST)",
+        "logoteam1": "punjab.png",
+        "logoteam2": "rc.png"
+    }];
+
+
+
 });

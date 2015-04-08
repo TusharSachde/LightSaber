98a953cd99c13b918784e812a94f62426d18bcd6
Tushar <tushar@wohlig.com>
2015-04-09 03:24:52 +0530

    chanhe

--- js/app.js
+++ js/app.js
@@ -86,7 +86,7 @@ predictoapp.config(function($stateProvider, $urlRouterProvider, $ionicConfigProv
             }
         });
     // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/login');
+    $urlRouterProvider.otherwise('/app/home');
 
 
 });

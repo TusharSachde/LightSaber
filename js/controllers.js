35820ef7587863c02c748927fc89a067afaaa426
Chintan Shah <chintan@wohlig.com>
2015-04-12 04:34:03 +0530

    offline

--- js/controllers.js
+++ js/controllers.js
@@ -68,73 +68,79 @@ angular.module('starter.controllers', ['myservices'])
 
 })
 
-.controller('OfflineCtrl', function ($scope, $ionicModal, $timeout,$location) {
+.controller('OfflineCtrl', function ($scope, $ionicModal, $timeout, $location,$ionicLoading) {
 
     function ononline() {
         $location.url("/app/home");
     };
     document.addEventListener("online", ononline, false);
     $scope.tryagain = function () {
+        
         ononline();
     };
+    $ionicLoading.hide();
 
 })
 
 
 .controller('HomeCtrl', function ($scope, $ionicModal, $timeout, MyServices, $location, $ionicLoading, $anchorScroll, $ionicScrollDelegate) {
 
-        $ionicLoading.show({
-            template: 'please wait...'
-        });
+    $ionicLoading.show({
+        template: 'please wait...'
+    });
 
 
 
 
 
-        //  AUTHENTICATE USER
+    //  AUTHENTICATE USER
 
 
 
-        if (user) {
-            console.log("Stay at home");
-        } else {
-            $location.url("/login");
+    if (user) {
+        console.log("Stay at home");
+    } else {
+        $location.url("/login");
+    }
+
+
+
+    //  GET LIST OF PREDICTIONS FOR IPL
+
+    var getpredictionssuccess = function (data, status) {
+        $scope.predictions = data;
+        console.log(data);
+        $ionicLoading.hide();
+        var i = 0;
+        for (i = 0; i < data.length; i++) {
+            if (data[i].status == 1) {
+                break;
+            }
         }
 
+        $ionicScrollDelegate.scrollTo(0, 134 * i, true);
+    };
+    var getpredictionserror = function () {
+        $ionicLoading.hide();
+        $location.url("/offline");
+    };
+    MyServices.getpredictions().success(getpredictionssuccess).error(getpredictionserror);
 
-
-        //  GET LIST OF PREDICTIONS FOR IPL
-
-        var getpredictionssuccess = function (data, status) {
-            $scope.predictions = data;
-            console.log(data);
-            $ionicLoading.hide();
-            var i = 0;
-            for (i = 0; i < data.length; i++) {
-                if (data[i].status == 1) {
-                    break;
-                }
-            }
-
-            $ionicScrollDelegate.scrollTo(0, 134 * i, true);
-        };
-        MyServices.getpredictions().success(getpredictionssuccess);
-
-        var giveidvar = 0;
-        $scope.giveid = function (status) {
-            console.log(giveidvar);
-            if (giveidvar == 0) {
-                if (status == 1) {
-                    console.log("mila");
-                    return true;
-                };
-                giveidvar = 1;
+    var giveidvar = 0;
+    $scope.giveid = function (status) {
+        console.log(giveidvar);
+        if (giveidvar == 0) {
+            if (status == 1) {
+                console.log("mila");
+                return true;
             };
-            return false;
+            giveidvar = 1;
         };
+        return false;
+    };
 
 
-    })
+})
     .controller('HistoryCtrl', function ($scope, $ionicModal, $timeout, $ionicLoading, MyServices, $location) {
 
         //  IONIC LOADING
@@ -200,131 +206,132 @@ angular.module('starter.controllers', ['myservices'])
 
 .controller('PredictCtrl', function ($scope, $ionicModal, $timeout, $stateParams, MyServices, $ionicLoading, $location, $ionicPopup) {
 
-        $scope.clickr1 = false;
-        $scope.clickr2 = false;
+    $scope.clickr1 = false;
+    $scope.clickr2 = false;
 
-        //  IONIC LOADING
+    //  IONIC LOADING
 
-        $ionicLoading.show({
-            template: 'Please wait...'
-        });
+    $ionicLoading.show({
+        template: 'Please wait...'
+    });
 
 
-        //  AUTHENTICATE USER
-        if (user) {
-            console.log("Stay at home");
-        } else {
-            $location.url("/login");
+    //  AUTHENTICATE USER
+    if (user) {
+        console.log("Stay at home");
+    } else {
+        $location.url("/login");
+    }
+
+
+    var predictiondata = {};
+    predictiondata.prediction = $stateParams.id;
+    predictiondata.user = user.id;
+
+
+
+    //HASH DESIGN
+    var hashdesign = function (data, string, index) {
+        for (var k = 0; k < data.length; k++) {
+            var string = string.replace("#" + data[k], "<span class='positive'>#" + data[k] + "</span>");
+        };
+        if ($scope.predictdata.tweets) {
+            $scope.predictdata.tweets.statuses[index].text = string;
         }
+    };
 
+    var tweeter = function () {
+        console.log($scope.tweets);
+        if ($scope.tweets) {
+            for (var i = 0; i < $scope.tweets.length; i++) {
+                //GET STRING
+                var string = $scope.tweets[i].text;
 
-        var predictiondata = {};
-        predictiondata.prediction = $stateParams.id;
-        predictiondata.user = user.id;
-
-
-
-        //HASH DESIGN
-        var hashdesign = function (data, string, index) {
-            for (var k = 0; k < data.length; k++) {
-                var string = string.replace("#" + data[k], "<span class='positive'>#" + data[k] + "</span>");
-            };
-            if ($scope.predictdata.tweets) {
-                $scope.predictdata.tweets.statuses[index].text = string;
-            }
-        };
-
-        var tweeter = function () {
-            console.log($scope.tweets);
-            if ($scope.tweets) {
-                for (var i = 0; i < $scope.tweets.length; i++) {
-                    //GET STRING
-                    var string = $scope.tweets[i].text;
-
-                    var hastagarray = $scope.tweets[i].entities.hashtags;
-                    //CLEAN INDICES ARAY
-                    var textarray = [];
-                    //ITERATE HASTAG
-                    for (var j = 0; j < hastagarray.length; j++) {
-                        //var indices = hastagarray[j].indices;
-                        //indicesaray.push(indices);
-                        var text = hastagarray[j].text;
-                        textarray.push(text);
-                    };
-                    hashdesign(textarray, string, i);
+                var hastagarray = $scope.tweets[i].entities.hashtags;
+                //CLEAN INDICES ARAY
+                var textarray = [];
+                //ITERATE HASTAG
+                for (var j = 0; j < hastagarray.length; j++) {
+                    //var indices = hastagarray[j].indices;
+                    //indicesaray.push(indices);
+                    var text = hastagarray[j].text;
+                    textarray.push(text);
                 };
-            }
-        };
+                hashdesign(textarray, string, i);
+            };
+        }
+    };
 
-        var getpredictionforusersuccess = function (data, status) {
+    var getpredictionforusersuccess = function (data, status) {
+        console.log(data);
+        $scope.predictdata = data;
+        if ($scope.predictdata.predicted == $scope.predictdata.team1id) {
+            $scope.clickr1 = true;
+        }
+        if ($scope.predictdata.predicted == $scope.predictdata.team2id) {
+            $scope.clickr2 = true;
+        }
+        if ($scope.predictdata.tweets) {
+            $scope.tweets = $scope.predictdata.tweets.statuses;
+        }
+        tweeter();
+        $ionicLoading.hide();
+
+    };
+    //GET ALL DETAILS INITIALLY
+    MyServices.getpredictionforuser(predictiondata).success(getpredictionforusersuccess);
+
+
+    //USER PREDICTS
+    var userpredictssuccess = function (data, count) {
+        if (count == $scope.countforpredict) {
             console.log(data);
-            $scope.predictdata = data;
-            if ($scope.predictdata.predicted == $scope.predictdata.team1id) {
+            getpredictionforusersuccess(data);
+        }
+    };
+    $scope.countforpredict = 0;
+    $scope.userpredict = function (status, id, tick) {
+        if (status == 1) {
+            //CLOSE LAST CALL
+            if (tick == 1) {
                 $scope.clickr1 = true;
-            }
-            if ($scope.predictdata.predicted == $scope.predictdata.team2id) {
+                $scope.clickr2 = false;
+            } else if (tick == 2) {
+                $scope.clickr1 = false;
                 $scope.clickr2 = true;
-            }
-            if ($scope.predictdata.tweets) {
-                $scope.tweets = $scope.predictdata.tweets.statuses;
-            }
-            tweeter();
-            $ionicLoading.hide();
-
-        };
-        //GET ALL DETAILS INITIALLY
-        MyServices.getpredictionforuser(predictiondata).success(getpredictionforusersuccess);
-
-
-        //USER PREDICTS
-        var userpredictssuccess = function (data, count) {
-            if (count == $scope.countforpredict) {
-                console.log(data);
-                getpredictionforusersuccess(data);
-            }
-        };
-        $scope.countforpredict = 0;
-        $scope.userpredict = function (status, id, tick) {
-            if (status == 1) {
-                //CLOSE LAST CALL
-                if (tick == 1) {
-                    $scope.clickr1 = true;
-                    $scope.clickr2 = false;
-                } else if (tick == 2) {
-                    $scope.clickr1 = false;
-                    $scope.clickr2 = true;
-                };
-
-                var userpredictsdata = {};
-                userpredictsdata.prediction = predictiondata.prediction;
-                userpredictsdata.team = id;
-                userpredictsdata.user = user.id;
-                MyServices.userpredicts(userpredictsdata, ++$scope.countforpredict, userpredictssuccess);
-                $scope.showPopup();
             };
+
+            var userpredictsdata = {};
+            userpredictsdata.prediction = predictiondata.prediction;
+            userpredictsdata.team = id;
+            userpredictsdata.user = user.id;
+            MyServices.userpredicts(userpredictsdata, ++$scope.countforpredict, userpredictssuccess);
+            $scope.showPopup();
         };
+    };
 
-        $scope.showPopup = function () {
-            $scope.data = {}
+    $scope.showPopup = function () {
+        $scope.data = {}
 
-            // An elaborate, custom popup
-            var myPopup = $ionicPopup.show({
-                template: '<p class="text-center"><i class="icon ln-thumbs-up bigr positive"></i><br>Your prediction has been recorded, points will be updated after the match is over !</p>',
-                title: 'Thank You!',
-                scope: $scope,
+        // An elaborate, custom popup
+        var myPopup = $ionicPopup.show({
+            template: '<p class="text-center"><i class="icon ln-thumbs-up bigr positive"></i><br>Your prediction has been recorded, points will be updated after the match is over !</p>',
+            title: 'Thank You!',
+            scope: $scope,
 
-            });
-            $timeout(function () {
-                myPopup.close(); //close the popup after 3 seconds for some reason
-            }, 3000);
-        };
+        });
+        $timeout(function () {
+            myPopup.close(); //close the popup after 3 seconds for some reason
+        }, 3000);
+    };
 
-    })
-    .controller('SidemenuCtrl', function ($scope, $ionicModal, $timeout, MyServices, $location) {
+})
+    .controller('SidemenuCtrl', function ($scope, $ionicModal, $timeout, MyServices, $location, $ionicLoading) {
 
         var getuserdetailssuccess = function (data, status) {
             $scope.userdetails = data;
         };
+        
 
         function onOffline() {
             $location.url("/offline");
@@ -332,6 +339,7 @@ angular.module('starter.controllers', ['myservices'])
         $scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
             document.addEventListener("offline", onOffline, false);
             MyServices.getuserdetails().success(getuserdetailssuccess);
+            
         });
 
 
